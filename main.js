69a02491a82629b70df61acb1d927bb1840a9903
Eyder ACM <eyderacm@gmail.com>
2018-12-04 22:32:17 -0600

    Add class to every element

---
+++ main.js
@@ -0,0 +1,9 @@
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+
+document.body.append(highlight);
+
+function highlightLink() {
+    console.log('highlight');
+}
