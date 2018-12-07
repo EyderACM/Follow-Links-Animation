d4dd7b9e0732a4d8da6f0b7ded931418fdd65e62
Eyder ACM <eyderacm@gmail.com>
2018-12-06 22:47:51 -0600

    Dinamic highlight selection

--- main.js
+++ main.js
@@ -5,7 +5,18 @@ highlight.classList.add('highlight');
 document.body.append(highlight);
 
 function highlightLink() {
-    console.log(this);
+    const linkCoords = this.getBoundingClientRect();
+    console.log(linkCoords);
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height, 
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX
+    }
+
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 
 triggers.forEach(x => x.addEventListener('mouseenter', highlightLink));
